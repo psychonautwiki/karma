b3f795415e74ea3117f2fc9917ef10077408573b
Kenan Sulayman <kenan@sly.mn>
2017-02-05 06:03:26 +0100

    Port train method

--- poc/reference.js
+++ poc/reference.js
@@ -1,39 +1,30 @@
 const HMM = ((() => {
     const ARR = (typeof Float64Array) == 'function' ? Float64Array : Array;
 
-    const _choose = function _choose(a, ao, as, al){
-        let i;
-        let x = Math.random();
-        for(i=0; i<al; i++){
-            x -= a[ao+i*as];
-            if(x < 0) return i;
-        }
-        return al-1;
-    };
-
     class HMM {
         constructor(N, O) {
-            this.N = N;this.O = O;
+            this.N = N;
+            this.O = O;
 
             this.init_probs = new ARR(N);
-            this.next_probs = new ARR(N*N); // curr*N + next
-            this.out_probs = new ARR(N*O); // out*N + curr
+            this.next_probs = new ARR(N * N); // curr*N + next
+            this.out_probs = new ARR(N * O); // out*N + curr
 
             let obj;
             let i;
             let j;
-            for(i=0; i<N*N; i++) this.next_probs[i] = 1/N;
-            for(i=0; i<N*O; i++) this.out_probs[i] = 1/O;
-            for(i=0; i<N; i++) this.init_probs[i] = 1/N;
+            for (i = 0; i < N; i++) this.init_probs[i] = 1 / N;
+            for (i = 0; i < N * N; i++) this.next_probs[i] = 1 / N;
+            for (i = 0; i < N * O; i++) this.out_probs[i] = 1 / O;
 
             // for computing
             this._resize_tmp(16);
         }
 
         _resize_tmp(s) {
-            if(s <= this._max_s) return;
+            if (s <= this._max_s) return;
             this._max_s = s;
-            this._tmp = new ARR(this.N*(3*this._max_s+this.N));
+            this._tmp = new ARR(this.N * (3 * this._max_s + this.N));
         }
 
         randomize() {
@@ -42,39 +33,19 @@ const HMM = ((() => {
             let k;
             let x;
             const N = this.N;
-            for(k=0; k<3*N; k++){
-                i = 0|Math.random()*N;
-                j = 0|Math.random()*N;
-                if(i==j) continue;
-                if(this.init_probs[i] + this.init_probs[j] >= 1) continue;
-                x = Math.random()*this.init_probs[i];
+            for (k = 0; k < 3 * N; k++) {
+                i = 0 | Math.random() * N;
+                j = 0 | Math.random() * N;
+                if (i == j) continue;
+                if (this.init_probs[i] + this.init_probs[j] >= 1) continue;
+                x = Math.random() * this.init_probs[i];
                 this.init_probs[i] -= x;
                 this.init_probs[j] += x;
             }
         }
 
-        simulate(in_state) {
-            const N = this.N;
-            const O = this.O;
-            if(in_state == null) in_state = -1;
-            const next_state = in_state == -1 ? _choose(this.init_probs, 0, 1, N) : _choose(this.next_probs, in_state*N, 1, N);
-            const next_output = _choose(this.out_probs, next_state, N, O);
-            return [next_state, next_output];
-        }
-
-        generate(process) {
-            let curr_state = -1;
-            const result_str = [];
-            for(let i=0;;i++){
-                const v = this.simulate(curr_state);
-                curr_state = v[0];
-                result_str.push(v[1]);
-                if(!process(v[1], i)) return result_str;
-            }
-        }
-
         evaluate(outputs) {
-            if(outputs.length == 0) return 1;
+            if (outputs.length == 0) return 1;
 
             const N = this.N;
             const init_probs = this.init_probs;
@@ -91,109 +62,161 @@ const HMM = ((() => {
             let sum;
             let output;
 
-            for(i=0,j=outputs[0]*N; i<N; i++,j++) alphas[i] = init_probs[i] * out_probs[j];
+            for (i = 0, j = outputs[0] * N; i < N; i++, j++)
+                alphas[i] = init_probs[i] * out_probs[j];
 
             for(t=1; t<outputs.length; t++){
                 output = outputs[t];
-                for(j=0,k=output*N; j<N; j++,k++){
-                    for(sum=i=0,l=j; i<N; i++, l+=N) sum += alphas[i] * next_probs[l];
+
+                k=output*N;
+
+                for(j=0; j<N; j++){
+                    l=j;
+
+                    sum = 0;
+
+                    for(i=0; i<N; i++) {
+                        sum += alphas[i] * next_probs[l];
+
+                        l = l + N;
+                    }
+
                     alphas[N+j] = sum * out_probs[k];
+
+                    k = k + 1;
                 }
+
                 if(++t >= outputs.length) break;
 
                 output = outputs[t];
-                for(j=0,k=output*N; j<N; j++,k++){
-                    for(sum=i=0,l=j; i<N; i++, l+=N) sum += alphas[N+i] * next_probs[l];
+
+                k=output*N;
+
+                for(j=0; j<N; j++){
+                    l = j;
+
+                    sum = 0;
+
+                    for(i=0; i<N; i++, l+=N)
+                        sum += alphas[N+i] * next_probs[l];
+
                     alphas[j] = sum * out_probs[k];
+
+                    k++;
                 }
             }
 
-            if(t&1) for(sum=i=0; i<N; i++) sum += alphas[i];
-            else for(sum=i=0; i<N; i++) sum += alphas[N+i];
+            if (t & 1)
+                for (sum = i = 0; i < N; i++) sum += alphas[i];
+            else
+                for (sum = i = 0; i < N; i++) sum += alphas[N + i];
             return sum;
         }
 
         train(outputs, rate) {
-            if(rate == null) rate = .05;
+            if (rate == null) rate = .05;
 
             let i;
             let j;
             let k;
             let l;
             let sum;
-            const init = this.init_probs;
+            const init_probs = this.init_probs;
             const next_probs = this.next_probs;
             const out_probs = this.out_probs;
-
-            const n_next_probs = this._n_next_probs;
 
             const N = this.N;
             const O = this.O;
             const S = outputs.length;
-            const outputs_N = outputs.map(v => v*N);
+            const outputs_N = outputs.map(v => v * N);
 
             this._resize_tmp(S);
-            const BETA = N*S;
-            const GAMMA = 2*BETA;
-            const KAPPA = 3*BETA;
-            const arr = this._tmp;
+            const BETA = N * S;
+            const GAMMA = 2 * BETA;
+            const KAPPA = 3 * BETA;
 
             // Expectation - step 1 (computing alpha and beta)
-            for(j=0; j<N; j++) arr[j] = init[j]*out_probs[j+outputs_N[0]];
-            for(i=1; i<S; i++){
-                for(j=l=0; j<N; j++){
-                    for(k=sum=0; k<N; k++,l++){
-                        arr[KAPPA+l] = 0; // init kappa here
-                        sum += arr[(i-1)*N+k]*next_probs[k*N+j];
+            for (j = 0; j < N; j++) this._tmp[j] = init_probs[j] * out_probs[j + outputs_N[0]];
+            for (i = 1; i < S; i++) {
+                for (j = l = 0; j < N; j++) {
+                    for (k = sum = 0; k < N; k++) {
+                        this._tmp[KAPPA + l] = 0; // init kappa here
+                        sum += this._tmp[(i - 1) * N + k] * next_probs[k * N + j];
                     }
-                    arr[i*N+j] = sum*out_probs[j+outputs_N[i]];
+                    this._tmp[i * N + j] = sum * out_probs[j + outputs_N[i]];
+
+                    l = l + 1;
                 }
             }
-            for(j=0,l=(S-1)*N; j<N; j++,l++){
-                arr[BETA+l] = 1;
+
+            l = (S - 1) * N;
+
+            for (j = 0; j < N; j++) {
+                this._tmp[BETA + l] = 1;
+
+                l = l + 1;
             }
-            for(i=S-1; i-->0; ){
-                for(j=0,l=i*N; j<N; j++, l++){
-                    arr[BETA+l] = 0;
-                    for(k=0; k<N; k++)
-                        arr[BETA+l] += next_probs[j*N+k]*out_probs[k+outputs_N[i+1]]*arr[BETA+(i+1)*N+k];
+
+            for (i = S - 1; i-- > 0;) {
+                l = i * N;
+
+                for (j = 0; j < N; j++) {
+                    this._tmp[BETA + l] = 0;
+                    for (k = 0; k < N; k++)
+                        this._tmp[BETA + l] += next_probs[j * N + k] * out_probs[k + outputs_N[i + 1]] * this._tmp[BETA + (i + 1) * N + k];
+
+                    l = l + 1;
                 }
             }
 
             // Expectation - step 2 (computing gamma and kappa)
-            for(i=0; i<S; i++){
-                for(k=sum=0; k<N; k++) sum += arr[i*N+k]*arr[BETA+i*N+k];
-                for(j=0; j<N; j++){
-                    arr[GAMMA+i*N+j] = arr[i*N+j]*arr[BETA+i*N+j]/sum;
+            for (i = 0; i < S; i++) {
+                sum = 0;
+
+                for (k = 0; k < N; k++) {
+                    sum += this._tmp[i * N + k] * this._tmp[BETA + i * N + k];
                 }
-                if(i==S-1) break;
-                for(j=sum=0; j<N; j++) for(k=0; k<N; k++){
-                    sum += arr[i*N+j]*next_probs[j*N+k]*out_probs[k+outputs_N[i+1]]*arr[BETA+(i+1)*N+k];
+
+                for (j = 0; j < N; j++) {
+                    this._tmp[GAMMA + i * N + j] = this._tmp[i * N + j] * this._tmp[BETA + i * N + j] / sum;
                 }
-                for(l=j=0; j<N; j++) for(k=0; k<N; k++,l++){
-                    arr[KAPPA+l] += arr[i*N+j]*next_probs[l]*out_probs[k+outputs_N[i+1]]*arr[BETA+(i+1)*N+k]/sum;
+                if (i == S - 1) break;
+                for (j = sum = 0; j < N; j++) {
+                    for (k = 0; k < N; k++) {
+                        sum += this._tmp[i * N + j] * next_probs[j * N + k] * out_probs[k + outputs_N[i + 1]] * this._tmp[BETA + (i + 1) * N + k];
+                    }
+                }
+
+                for (l = j = 0; j < N; j++) {
+                    for (k = 0; k < N; k++, l++) {
+                        this._tmp[KAPPA + l] += this._tmp[i * N + j] * next_probs[l] * out_probs[k + outputs_N[i + 1]] * this._tmp[BETA + (i + 1) * N + k] / sum;
+                    }
                 }
             }
 
             // Maximum likelihood
-            let x;
-
-            const p=[];
             let del;
-            for(l=i=0; i<N; i++){
-                for(k=sum=0; k<S-1; k++) sum += arr[GAMMA+k*N+i];
-                for(j=0; j<N; j++,l++){
-                    del = arr[KAPPA+l]/sum-next_probs[l];
-                    next_probs[l] += del*rate;
+            for (l = i = 0; i < N; i++) {
+                for (k = sum = 0; k < S - 1; k++) sum += this._tmp[GAMMA + k * N + i];
+                for (j = 0; j < N; j++, l++) {
+                    del = this._tmp[KAPPA + l] / sum - next_probs[l];
+                    next_probs[l] += del * rate;
                 }
-                sum += arr[GAMMA+(S-1)*N+i];
-                for(j=0; j<O; j++){
-                    for(k=x=0; k<S; k++) if(outputs[k]==j) x += arr[GAMMA+k*N+i];
-                    del = x/sum-out_probs[i+j*N];
-                    out_probs[i+j*N] += del*rate;
+                sum += this._tmp[GAMMA + (S - 1) * N + i];
+
+                let x = 0;
+
+                for (j = 0; j < O; j++) {
+                    for (k = x = 0; k < S; k++)
+                        if (outputs[k] == j) {
+                            debugger;
+                            x += this._tmp[GAMMA + k * N + i];
+                        }
+                    del = x / sum - out_probs[i + j * N];
+                    out_probs[i + j * N] += del * rate;
                 }
-                del = arr[GAMMA+i]-init[i];
-                init[i] += del*rate;
+                del = this._tmp[GAMMA + i] - init_probs[i];
+                init_probs[i] += del * rate;
             }
         }
     }
@@ -207,11 +230,21 @@ const HMM = ((() => {
     return HMM;
 }))();
 
-if(typeof module !== 'undefined' && typeof require !== 'undefined'){
+if (typeof module !== 'undefined' && typeof require !== 'undefined') {
     module.exports = HMM;
 }
 
-const testPool = ["1,4-Butanediol","1P-ETH-LAD","1P-LSD","2-Aminoindane","2-FA","2-FMA","2-Fluorodeschloroketamine","2-Oxo-PCE","2-methyl-2-butanol","25B-NBOMe","25C-NBOMe","25I-NBOMe","25N-NBOMe","2C-B","2C-B-FLY","2C-C","2C-D","2C-E","2C-H","2C-I","2C-P","2C-T-2","2C-T-7","3-FA","3-FEA","3-FPM","3-MMC","3-MeO-PCE","3-MeO-PCMo","3-MeO-PCP","4-AcO-DET","4-AcO-DMT","4-AcO-DiPT","4-AcO-MET","4-AcO-MiPT","4-FA","4-FMA","4-HO-DET","4-HO-DPT","4-HO-DiPT","4-HO-EPT","4-HO-MET","4-HO-MPT","4-HO-MiPT","4-MeO-PCP","4F-EPH","5-APB","5-Hydroxytryptophan","5-MAPB","5-MeO-DALT","5-MeO-DMT","5-MeO-DiBF","5-MeO-DiPT","5-MeO-MiPT","5F-AKB48","5F-PB-22","6-APB","6-APDB","A-PHP","A-PVP","AB-FUBINACA","AL-LAD","ALD-52","Acetylfentanyl","Alcohol","Alpha-GPC","Alprazolam","Amphetamine","Armodafinil","Ayahuasca","Benzydamine","Bufotenin","Buprenorphine","Butylone","Caffeine","Cannabis","Carisoprodol","Choline bitartrate","Citicoline","Clonazepam","Clonazolam","Cocaine","Codeine","Coluracetam","Creatine","DET","DMT","DOB","DOC","DOI","DOM","DPT","DXM & DPH in combination","Dehydroxyfluorafinil","Deschloroetizolam","Deschloroketamine","Desoxypipradol","Dextromethorphan","Dextropropoxyphene","DiPT","Diazepam","Diclazepam","Dihydrocodeine","Diphenhydramine","Diphenidine","ETH-LAD","Efavirenz","Ephenidine","Escaline","Ethyl-Hexedrone","Ethylcathinone","Ethylone","Ethylphenidate","Etizolam","F-Phenibut","Fentanyl","Flubromazepam","Flubromazolam","GBL","GHB","Gabapentin","Haloperidol","Heroin","Hydrocodone","Hydromorphone","Ibogaine","Isopropylphenidate","JWH-073","Ketamine","Kratom","LSA","LSD","LSZ","Lisdexamfetamine","Lorazepam","MCPP","MDA","MDAI","MDEA","MDMA","MDPV","MET","MPT","Melatonin","Mephedrone","Methadone","Methallylescaline","Methamphetamine","Methaqualone","Methiopropamine","Methoxetamine","Methoxphenidine","Methylnaphthidate","Methylone","Methylphenidate","Metizolam","Mexedrone","MiPT","Mirtazapine","Modafinil","N-Acetylcysteine","NM-2-AI","Naloxone","Nicotine","Nifoxipam","Nitrous Oxide","Noopept","O-Desmethyltramadol","Oxiracetam","Oxycodone","Oxymorphone","PCP","PMMA","PRO-LAD","Pentobarbital","Pethidine","Phenibut","Phenobarbital","Piracetam","Pramiracetam","Pregabalin","Prolintane","Propylhexedrine","Proscaline","Psilocin","Pyrazolam","Quetiapine","RTI-111","Risperidone","STS-135","Salvinorin A","Secobarbital","Sufentanil","THJ-018","THJ-2201","TMA-2","TMA-6","Tapentadol","Temazepam","Theanine","Tianeptine","Tramadol","Tyrosine","U-47700","Zolpidem","Zopiclone"];
+var hmm = new HMM(20, 10);
+
+hmm.randomize();
+
+hmm.train([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+hmm.train([0, 1, 4, 5, 7, 9, 0, 4, 7, 8, 2], 0.05);
+
+console.log(hmm.evaluate([5, 6, 7, 8]))
+
+/*const testPool = ["1,4-Butanediol", "1P-ETH-LAD", "1P-LSD", "2-Aminoindane", "2-FA", "2-FMA", "2-Fluorodeschloroketamine", "2-Oxo-PCE", "2-methyl-2-butanol", "25B-NBOMe", "25C-NBOMe", "25I-NBOMe", "25N-NBOMe", "2C-B", "2C-B-FLY", "2C-C", "2C-D", "2C-E", "2C-H", "2C-I", "2C-P", "2C-T-2", "2C-T-7", "3-FA", "3-FEA", "3-FPM", "3-MMC", "3-MeO-PCE", "3-MeO-PCMo", "3-MeO-PCP", "4-AcO-DET", "4-AcO-DMT", "4-AcO-DiPT", "4-AcO-MET", "4-AcO-MiPT", "4-FA", "4-FMA", "4-HO-DET", "4-HO-DPT", "4-HO-DiPT", "4-HO-EPT", "4-HO-MET", "4-HO-MPT", "4-HO-MiPT", "4-MeO-PCP", "4F-EPH", "5-APB", "5-Hydroxytryptophan", "5-MAPB", "5-MeO-DALT", "5-MeO-DMT", "5-MeO-DiBF", "5-MeO-DiPT", "5-MeO-MiPT", "5F-AKB48", "5F-PB-22", "6-APB", "6-APDB", "A-PHP", "A-PVP", "AB-FUBINACA", "AL-LAD", "ALD-52", "Acetylfentanyl", "Alcohol", "Alpha-GPC", "Alprazolam", "Amphetamine", "Armodafinil", "Ayahuasca", "Benzydamine", "Bufotenin", "Buprenorphine", "Butylone", "Caffeine", "Cannabis", "Carisoprodol", "Choline bitartrate", "Citicoline", "Clonazepam", "Clonazolam", "Cocaine", "Codeine", "Coluracetam", "Creatine", "DET", "DMT", "DOB", "DOC", "DOI", "DOM", "DPT", "DXM & DPH in combination", "Dehydroxyfluorafinil", "Deschloroetizolam", "Deschloroketamine", "Desoxypipradol", "Dextromethorphan", "Dextropropoxyphene", "DiPT", "Diazepam", "Diclazepam", "Dihydrocodeine", "Diphenhydramine", "Diphenidine", "ETH-LAD", "Efavirenz", "Ephenidine", "Escaline", "Ethyl-Hexedrone", "Ethylcathinone", "Ethylone", "Ethylphenidate", "Etizolam", "F-Phenibut", "Fentanyl", "Flubromazepam", "Flubromazolam", "GBL", "GHB", "Gabapentin", "Haloperidol", "Heroin", "Hydrocodone", "Hydromorphone", "Ibogaine", "Isopropylphenidate", "JWH-073", "Ketamine", "Kratom", "LSA", "LSD", "LSZ", "Lisdexamfetamine", "Lorazepam", "MCPP", "MDA", "MDAI", "MDEA", "MDMA", "MDPV", "MET", "MPT", "Melatonin", "Mephedrone", "Methadone", "Methallylescaline", "Methamphetamine", "Methaqualone", "Methiopropamine", "Methoxetamine", "Methoxphenidine", "Methylnaphthidate", "Methylone", "Methylphenidate", "Metizolam", "Mexedrone", "MiPT", "Mirtazapine", "Modafinil", "N-Acetylcysteine", "NM-2-AI", "Naloxone", "Nicotine", "Nifoxipam", "Nitrous Oxide", "Noopept", "O-Desmethyltramadol", "Oxiracetam", "Oxycodone", "Oxymorphone", "PCP", "PMMA", "PRO-LAD", "Pentobarbital", "Pethidine", "Phenibut", "Phenobarbital", "Piracetam", "Pramiracetam", "Pregabalin", "Prolintane", "Propylhexedrine", "Proscaline", "Psilocin", "Pyrazolam", "Quetiapine", "RTI-111", "Risperidone", "STS-135", "Salvinorin A", "Secobarbital", "Sufentanil", "THJ-018", "THJ-2201", "TMA-2", "TMA-6", "Tapentadol", "Temazepam", "Theanine", "Tianeptine", "Tramadol", "Tyrosine", "U-47700", "Zolpidem", "Zopiclone"];
 
 const mapToTestPool = vec => vec.map(substance => testPool.indexOf(substance));
 
@@ -259,4 +292,4 @@ const mapToTestPool = vec => vec.map(substance => testPool.indexOf(substance));
         hmm.evaluate(mapToTestPool(["Ethylone", "Methiopropamine", "Nicotine", "A-PVP"])),
         hmm.evaluate(mapToTestPool(["2C-D", "DPT", "Efavirenz", "2C-D"]))
     );
-}
+}*/
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate rand;
 
 use rand::Rng;
 
-struct Karma {
+pub struct Karma {
     // states
     N: i64,
     // items
@@ -19,7 +19,7 @@ struct Karma {
 }
 
 impl Karma {
-    fn new(states: i64, items: i64) -> Karma {
+    pub fn new(states: i64, items: i64) -> Karma {
         let states_sq = states * states;
         let states_items = states * items;
 
@@ -48,13 +48,15 @@ impl Karma {
     }
 
     fn _resize_tmp(&mut self, new_size: i64) {
-        if new_size > self._max_size {
-            self._max_size = new_size;
-            self._tmp = Box::new(vec!(0f64; (self.N * (3 * new_size + self.N)) as usize));
+        if new_size <= self._max_size {
+            return;
         }
+
+        self._max_size = new_size;
+        self._tmp = Box::new(vec!(0f64; (self.N * (3 * new_size + self.N)) as usize));
     }
 
-    fn randomize(&mut self) {
+    pub fn randomize(&mut self) {
         let states = self.N;
 
         for k in 0..(3 * states) {
@@ -72,7 +74,7 @@ impl Karma {
         }
     }
 
-    fn evaluate(&mut self, outputs: Vec<i64>) -> f64 {
+    pub fn evaluate(&mut self, outputs: Vec<i64>) -> f64 {
         if outputs.len() == 0 {
             return 1.0f64;
         }
@@ -91,7 +93,7 @@ impl Karma {
 
         let mut t = 1;
 
-        while(t < outputs.len()) {
+        while t < outputs.len() {
             let output = outputs[t as usize];
 
             let mut k = (output * states) as usize;
@@ -125,7 +127,7 @@ impl Karma {
             for j in 0..states {
                 let mut l = j;
 
-                sum = 0;
+                sum = 0f64;
 
                 for i in 0..states {
                     sum = sum + (self._tmp[(states + i) as usize] * self.next_probs[l as usize]);
@@ -141,8 +143,10 @@ impl Karma {
             t = t + 1;
         }
 
+        let mut sum = 0f64;
+
         for i in 0..states {
-            let offset = match (t & 1) {
+            let offset = match t & 1 {
                 1 => i,
                 _ => (states + i)
             };
@@ -153,6 +157,200 @@ impl Karma {
         sum
     }
 
+    pub fn train(&mut self, outputs: Vec<i64>, rate: Option<f64>) {
+        let rate = match rate {
+            Some(rate) => rate,
+            None => 0.05f64
+        };
+
+        let states = self.N;
+        let items = self.O;
+
+        let S = outputs.len() as i64;
+
+        let outputs_N: Vec<i64> = outputs.clone()
+                                         .into_iter()
+                                         .map(|x| x * states)
+                                         .collect();
+
+        self._resize_tmp(S);
+
+        let BETA: i64 = states * items;
+        let GAMMA = 2 * BETA;
+        let KAPPA = 3 * BETA;
+
+        // Expectation - step 1 (computing alpha and beta)
+
+        for j in 0..states {
+            let j = j as usize;
+
+            self._tmp[j] = self.init_probs[j] * self.out_probs[j + (outputs_N[0] as usize)];
+        }
+
+        for i in 1..S {
+            let mut l = 0i64;
+
+            println!("{:?}", self._tmp);
+
+            for j in 0..states {
+                let mut sum = 0f64;
+
+                for k in 0..states {
+                    self._tmp[(KAPPA + l) as usize] = 0f64;
+
+                    sum = sum
+                        + ((
+                            self._tmp[((i - 1) * states + k) as usize]
+                          * self.next_probs[(k * states + j) as usize]
+                        ) as f64);
+
+                    l = l + 1;
+                }
+
+                self._tmp[(i * states + j) as usize] =
+                      sum
+                    * self.out_probs[(j + outputs_N[i as usize]) as usize];
+            }
+
+            println!("{:?}", self._tmp);
+
+            l = (S - 1) * states;
+
+            for j in 0..states {
+                self._tmp[(BETA + l) as usize] = 1f64;
+
+                l = l + 1;
+            }
+
+            for i in (0..S-1).rev() {
+                l = i * states;
+
+                for j in 0..states {
+                    let index = (BETA + l) as usize;
+
+                    self._tmp[index] = 0f64;
+
+                    for k in 0..states {
+                        self._tmp[index] =
+                              self._tmp[index]
+                            + (
+                                  self.next_probs[(j * states + k) as usize]
+                                * self.out_probs[(k + outputs_N[(i + 1) as usize]) as usize]
+                                * self._tmp[(BETA + (i + 1) * states + k) as usize]
+                            );
+                    }
+
+                    l = l + 1;
+                }
+            }
+
+            // Expectation - step 2 (computing gamma and kappa)
+
+            for i in i..S {
+                let mut sum = 0f64;
+
+                for k in 0..states {
+                    sum = sum
+                          + (
+                               self._tmp[(i * states + k) as usize]
+                             * self._tmp[(BETA + i * states + k) as usize]
+                          );
+                }
+
+                for j in 0..states {
+                    self._tmp[(GAMMA + i * states + j) as usize] =
+                          self._tmp[(i * states + j) as usize]
+                        * self._tmp[(BETA + i * states + j) as usize]
+                        / sum;
+                }
+
+                if i == S - 1 {
+                    break;
+                }
+
+                let mut sum = 0f64;
+
+                for j in 0..states {
+                    for k in 0..states {
+                        sum = sum
+                            + (
+                                 self._tmp[(i * states + j) as usize]
+                               * self.next_probs[(j * states + k) as usize]
+                               * self.out_probs[(k + outputs_N[(i + 1) as usize]) as usize]
+                               * self._tmp[(BETA + (i + 1) * states + k) as usize]
+                            );
+                    }
+                }
+
+                let mut l = 0i64;
+
+                for j in 0..states {
+                    for k in 0..states {
+                        let index = (KAPPA + l) as usize;
+
+                        self._tmp[index] = self._tmp[index]
+                                         + (
+                                              self._tmp[(i * states + j) as usize]
+                                            * self.next_probs[l as usize]
+                                            * self.out_probs[(k + outputs_N[(i + 1) as usize]) as usize]
+                                            * self._tmp[(BETA + (i + 1) * states + k) as usize]
+                                            / sum
+                                          );
+
+                        l = l + 1;
+                    }
+                }
+
+                // Maximum likelihood
+
+                let mut l = 0i64;
+                let mut del = 0f64;
+
+                for i in 0..states {
+                    let mut sum = 0f64;
+
+                    for k in 0..(S - 1) {
+                        sum = sum + self._tmp[(GAMMA + k * states + i) as usize];
+                    }
+
+                    for j in 0..states {
+                        let index = l as usize;
+
+                        del = self._tmp[KAPPA as usize + index]
+                            / sum
+                            - self.next_probs[index];
+
+                        self.next_probs[index] = self.next_probs[index] + (del * rate);
+
+                        l = l + 1i64;
+                    }
+
+                    sum = sum + self._tmp[(GAMMA + (S - 1) * states + i) as usize];
+
+                    for j in 0..items {
+                        let mut x = 0f64;
+
+                        for k in 0..S {
+                            let output = outputs[k as usize];
+                            if output == j {
+                                x = x + self._tmp[(GAMMA + k * states + i) as usize];
+                            }
+                        }
+
+                        del = x / sum - self.out_probs[(i + j * states) as usize];
+
+                        let index = (i + j * states) as usize;
+
+                        self.out_probs[index] = self.out_probs[index] + (del * rate);
+                    }
+
+                    del = self._tmp[(GAMMA + i) as usize] - self.init_probs[i as usize];
+
+                    self.init_probs[i as usize] = self.init_probs[i as usize] + (del * rate);
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
